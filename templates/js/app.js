6ca5ad7334ccc9f6a3234f2d50941c175ca47b2a
Jairo Caro-Accino Viciana <Jairo Caro-Accino Viciana>
2016-02-12 12:35:35 +0100

    fail

--- templates/js/app.js
+++ templates/js/app.js
@@ -18,7 +18,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());{css}
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
